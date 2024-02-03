204f5dc5d7fa509029c67b16d3c28863b1ee7380
Stas Dubinin <dubinin.stas@gmail.com>
2024-02-03 08:28:33 +0400

    добавлен докек backend

---
+++ backend/src/auth/localGuard.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+
+@Injectable()
+export class LocalGuard extends AuthGuard('local') {}
